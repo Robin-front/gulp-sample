9dd54674a7fdfcd1f8877396dd212e85046f279c
robins <lb.robin1991@gmail.com>
2016-10-14 17:19:29 +0800

    add pxtorem

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 var minifycss = require('gulp-minify-css');
-var autoprefixer = require('gulp-autoprefixer');
+// var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var del = require('del');
 var base64 = require('gulp-base64');
@@ -13,16 +13,19 @@ var gulpif = require('gulp-if');
 var lazypipe = require('lazypipe');
 var cache = require('gulp-cached');
 var less = require('gulp-less');
+var pxtorem = require('postcss-pxtorem');
 var rev = require('gulp-rev');
 var revReplace = require('gulp-rev-replace');
 var connect = require('gulp-connect'); //文件服务器
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
 
 var paths = {
-  scripts: ['sc/js/**/*.js'],
-  font: 'sc/font/**/*',
-  imgs: 'sc/imgs/**/*',
-  css: 'sc/css/**/*.less',
-  html: 'sc/**/*.html'
+  scripts: ['scMobile/js/**/*.js'],
+  font: 'scMobile/font/**/*',
+  imgs: 'scMobile/imgs/**/*',
+  css: 'scMobile/css/**/*.less',
+  html: 'scMobile/**/*.html'
 };
 
 // Not all tasks need to use streams
@@ -55,13 +58,24 @@ gulp.task('imgs', function() {
     .pipe(connect.reload());
 });
 
-
+var processors = [
+        autoprefixer({
+            browsers: ['> 1%', 'IE 7', 'IE 8', 'ios 7', 'ios 8', 'android 4'],// css兼容的浏览器类型或平台
+        }),
+        // pxtorem({
+        //     rootValue: 20,
+        //     replace: true,
+        //     selectorBlackList: [/^html$/],
+        //     propWhiteList: [],
+        // })
+    ];
 var csspipe = lazypipe()
     .pipe(less)
-    .pipe(autoprefixer, {
-        browsers: ['last 2 versions', 'ios 7', 'ios 8', 'android 4', 'IE 7', 'IE 8'],// css兼容的浏览器类型或平台
-        cascade: false
-      })
+    .pipe(postcss, processors)
+    // .pipe(autoprefixer, {
+    //     browsers: ['> 1%', 'IE 7', 'IE 8', 'ios 7', 'ios 8', 'android 4'],// css兼容的浏览器类型或平台
+    //     cascade: false
+    //   })
     .pipe(base64, {
         baseDir: 'build/imgs/',
         maxImageSize: 15*1024, // 小于15K的图片转成base64,
@@ -104,9 +118,9 @@ gulp.task('font', function(){
 gulp.task('build', ['imgs', 'font', 'html']);
 
 gulp.task('watch', function() {
-  gulp.watch(paths.scripts, ['scripts', 'html']);
-  gulp.watch(paths.imgs, ['imgs']);
-  gulp.watch(paths.css, ['css', 'html']);
+  gulp.watch(paths.scripts, ['html']);
+  gulp.watch(paths.imgs, ['html']);
+  gulp.watch(paths.css, ['html']);
   gulp.watch(paths.html, ['html']);
 });
 
@@ -115,7 +129,7 @@ gulp.task('connect', function() {
     connect.server({
         root: 'build',
         livereload: true,
-        port: 8000,
+        port: 8888,
     });
 });
 
