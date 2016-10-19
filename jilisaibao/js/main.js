388d5b488a76a7d1704686610e45f50d68844709
robins <lb.robin1991@gmail.com>
2016-10-19 18:27:20 +0800

    done

--- jilisaibao/js/main.js
+++ jilisaibao/js/main.js
@@ -1,4 +1,8 @@
 $(function() {
+  var $screen = $('.J_screen');
+  var $body = $('body');
+  var $nav  = $('.J_nav');
+
   $(document).on('click', '.J_hongbao', function(){
     $('.J_dialog_rules').toggle();
     return false;
@@ -29,4 +33,54 @@ $(function() {
         pagination: '.swiper-pagination',
         paginationClickable: true
     });
+
+  var throttleV2 = function(fn, delay){
+   	var timer = null;
+   	return function(){
+   		var context = this, args = arguments;
+   		clearTimeout(timer);
+   		timer = setTimeout(function(){
+   			fn.apply(context, args);
+   		}, delay);
+   	};
+   };
+  var lastScrollTop = null;
+  $(window).scroll(throttleV2(function(){
+    var scrollTop = $body.scrollTop();
+    $screen.each(function(index){
+      var top = $(this).offset().top;
+
+      // 对应菜单
+      if ((top - scrollTop) < 350 && (top - scrollTop) > 0){
+        console.log('index', index);
+        $nav.find('.J_item').eq(index)
+          .addClass('current')
+          .siblings().removeClass('current');
+      }
+    });
+    lastScrollTop = scrollTop;
+  }, 500));
+
+  $nav.on('click', '.J_item', function(){
+    var index = $nav.children().index($(this));
+    var top = $screen.eq(index).offset().top - 66;
+    $(this).addClass('current').siblings().removeClass('current');
+    $body.animate({scrollTop: top}, 300);
+    return false;
+  });
+
+  $(document).on('click', '.J_next_screen', function(){
+    var $parent = $(this).closest('.J_screen');
+    var top = $parent.next('.J_screen').offset().top - 66;
+    $body.animate({scrollTop: top}, 300);
+  });
+
+  $(document).on('click', '.J_toTop', function(){
+    $body.animate({scrollTop: 0}, 300);
+  });
+
+  $(document).on('click', '.J_toBottom', function(){
+    var top = $screen.eq(-1).offset().top;
+    $body.animate({scrollTop: top}, 300);
+  });
 });
